a4153ba4b88c296a6b893f6db37891987547908a
AnnaForstman <anyta23345@gmail.com>
2024-12-10 16:17:24 +0300

    Add files via upload
    Исправление, цикл while

--- module_2_3.py
+++ module_2_3.py
@@ -4,6 +4,7 @@ while i < len(my_list):
     if my_list[i] > 0:
         print(my_list[i])
         i += 1
-    elif my_list[i] <= 0:
+    elif my_list[i] == 0:
         i += 1
-        continue
+    elif my_list[i] < 0:
+        break
